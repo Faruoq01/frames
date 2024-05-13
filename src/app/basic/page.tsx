cf2654edb10d5a1a86e206bb0ecb102e5d4f4bb9
Faruk <167757581+Faruoq04@users.noreply.github.com>
2024-05-13 13:56:33 +0100

    update again

--- src/app/basic/page.tsx
+++ src/app/basic/page.tsx
@@ -6,11 +6,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Basic Frame',
     description: 'A Farcaster Frame Demo',
-    images: [`https://images.unsplash.com/photo-1565772838491-cbeb32fac6ca?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D`],
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`],
   },
   other: {
     'fc:frame': 'vNext',
-    'fc:frame:image': `https://images.unsplash.com/photo-1565772838491-cbeb32fac6ca?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D`,
+    'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`,
     'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=1`,
     'fc:frame:button:1': 'Start',
   },
